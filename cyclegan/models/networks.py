2695e87546cf236aad1eb6511611ad5009a23aae
Tuntai Boriboonthana <s1629339@ed.ac.uk>
2019-06-15 17:03:21 +0100

    add squeezenet

--- cyclegan/models/networks.py
+++ cyclegan/models/networks.py
@@ -115,15 +115,28 @@ def define_C(output_nc, ndf, init_type='normal', gpu_ids=[]):
     #    netC = get_model('DTN', num_cls=10)
     #else:
     #    Exception('classifier only implemented for 32x32x3 images')
+    #=================================================================================================
+    num_classes = 31
+    netC = models.squeezenet1_0(pretrained=True)
+    set_parameter_requires_grad(netC, False)
+    netC.classifier[1] = nn.Conv2d(512, num_classes, kernel_size=(1, 1), stride=(1, 1))
+    netC.num_classes = num_classes
+    input_size = 224
+    print('Learnable parameters')
+    params_to_update = []
+    for name, param in netC.named_parameters():
+        if param.requires_grad == True:
+            params_to_update.append(param)
+            print("\t", name)
+    if len(gpu_ids) > 0:
+        assert(torch.cuda.is_available())
+        netC.to(gpu_ids[0])
+        netC = torch.nn.DataParallel(netC, gpu_ids)
+    return netC
+    # =================================================================================================
 
-    # model_ft = models.squeezenet1_0(pretrained=use_pretrained)
-    # set_parameter_requires_grad(model_ft, feature_extract)
-    # model_ft.classifier[1] = nn.Conv2d(512, num_classes, kernel_size=(1, 1), stride=(1, 1))
-    # model_ft.num_classes = num_classes
-    # input_size = 224
-
-    netC = Classifier(output_nc, ndf)
-    return init_net(netC, init_type, gpu_ids)
+    # netC = Classifier(output_nc, ndf)
+    # return init_net(netC, init_type, gpu_ids)
 
 def set_parameter_requires_grad(model, feature_extracting):
     if feature_extracting:
@@ -460,72 +473,72 @@ class PixelDiscriminator(nn.Module):
 #         out = self.after_linear(self.before_linear(x).view(bs, -1))
 #         return out
  #       return nn.functional.log_softmax(out, dim=1)
-class Classifier(nn.Module):
-    def __init__(self, input_nc, ndf, norm_layer=nn.BatchNorm2d):
-        super(Classifier, self).__init__()
-
-        num_classes = 31
-
-        # Convolution 1
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(3, 16, 3, 2, 1, bias=False),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Dropout(0.5, inplace=False),
-        )
-        # Convolution 2
-        self.conv2 = nn.Sequential(
-            nn.Conv2d(16, 32, 3, 1, 0, bias=False),
-            nn.BatchNorm2d(32),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Dropout(0.5, inplace=False),
-        )
-        # Convolution 3
-        self.conv3 = nn.Sequential(
-            nn.Conv2d(32, 64, 3, 2, 1, bias=False),
-            nn.BatchNorm2d(64),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Dropout(0.5, inplace=False),
-        )
-        # Convolution 4
-        self.conv4 = nn.Sequential(
-            nn.Conv2d(64, 128, 3, 1, 0, bias=False),
-            nn.BatchNorm2d(128),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Dropout(0.5, inplace=False),
-        )
-        # Convolution 5
-        self.conv5 = nn.Sequential(
-            nn.Conv2d(128, 256, 3, 2, 1, bias=False),
-            nn.BatchNorm2d(256),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Dropout(0.5, inplace=False),
-        )
-        # Convolution 6
-        self.conv6 = nn.Sequential(
-            nn.Conv2d(256, 512, 3, 1, 0, bias=False),
-            nn.BatchNorm2d(512),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Dropout(0.5, inplace=False),
-        )
-
-        self.global_avg = nn.AvgPool2d(kernel_size = (29, 29))
-
-        # aux-classifier fc
-        self.fc_aux = nn.Linear(512, num_classes)
-
-
-    def forward(self, input):
-        bs = input.size(0)
-        conv1 = self.conv1(input)
-        conv2 = self.conv2(conv1)
-        conv3 = self.conv3(conv2)
-        conv4 = self.conv4(conv3)
-        conv5 = self.conv5(conv4)
-        conv6 = self.conv6(conv5)
-        flat6 = self.global_avg(conv6)
-        flat6 = flat6.view(bs, -1)
-        fc_aux = self.fc_aux(flat6)
-
-        return fc_aux
+# class Classifier(nn.Module):
+#     def __init__(self, input_nc, ndf, norm_layer=nn.BatchNorm2d):
+#         super(Classifier, self).__init__()
+#
+#         num_classes = 31
+#
+#         # Convolution 1
+#         self.conv1 = nn.Sequential(
+#             nn.Conv2d(3, 16, 3, 2, 1, bias=False),
+#             nn.LeakyReLU(0.2, inplace=True),
+#             nn.Dropout(0.5, inplace=False),
+#         )
+#         # Convolution 2
+#         self.conv2 = nn.Sequential(
+#             nn.Conv2d(16, 32, 3, 1, 0, bias=False),
+#             nn.BatchNorm2d(32),
+#             nn.LeakyReLU(0.2, inplace=True),
+#             nn.Dropout(0.5, inplace=False),
+#         )
+#         # Convolution 3
+#         self.conv3 = nn.Sequential(
+#             nn.Conv2d(32, 64, 3, 2, 1, bias=False),
+#             nn.BatchNorm2d(64),
+#             nn.LeakyReLU(0.2, inplace=True),
+#             nn.Dropout(0.5, inplace=False),
+#         )
+#         # Convolution 4
+#         self.conv4 = nn.Sequential(
+#             nn.Conv2d(64, 128, 3, 1, 0, bias=False),
+#             nn.BatchNorm2d(128),
+#             nn.LeakyReLU(0.2, inplace=True),
+#             nn.Dropout(0.5, inplace=False),
+#         )
+#         # Convolution 5
+#         self.conv5 = nn.Sequential(
+#             nn.Conv2d(128, 256, 3, 2, 1, bias=False),
+#             nn.BatchNorm2d(256),
+#             nn.LeakyReLU(0.2, inplace=True),
+#             nn.Dropout(0.5, inplace=False),
+#         )
+#         # Convolution 6
+#         self.conv6 = nn.Sequential(
+#             nn.Conv2d(256, 512, 3, 1, 0, bias=False),
+#             nn.BatchNorm2d(512),
+#             nn.LeakyReLU(0.2, inplace=True),
+#             nn.Dropout(0.5, inplace=False),
+#         )
+#
+#         self.global_avg = nn.AvgPool2d(kernel_size = (29, 29))
+#
+#         # aux-classifier fc
+#         self.fc_aux = nn.Linear(512, num_classes)
+#
+#
+#     def forward(self, input):
+#         bs = input.size(0)
+#         conv1 = self.conv1(input)
+#         conv2 = self.conv2(conv1)
+#         conv3 = self.conv3(conv2)
+#         conv4 = self.conv4(conv3)
+#         conv5 = self.conv5(conv4)
+#         conv6 = self.conv6(conv5)
+#         flat6 = self.global_avg(conv6)
+#         flat6 = flat6.view(bs, -1)
+#         fc_aux = self.fc_aux(flat6)
+#
+#         return fc_aux
 
 
